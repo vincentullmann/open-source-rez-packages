bcfccdd2a0c3eef4107e35d1b648d7fb29efca4b
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2025-04-14 13:57:40 +0100

    add alembic-1.8.8

---
+++ alembic/1.8.8/package.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+name = 'alembic'
+
+version = '1.8.8'
+
+build_requires = ['cmake-3','devtoolset']
+
+requires = [
+    'boost-1.85',
+    'Imath-3',
+    'devtoolset',
+    'python-3.11'
+]
+
+variants = [['platform-linux', 'arch-x86_64']]
+
+def commands():
+    appendenv('LD_LIBRARY_PATH', '{root}/lib/')
+    appendenv('PATH', '{root}/bin/')
+    appendenv('PYTHONPATH', '{root}/lib/')
+

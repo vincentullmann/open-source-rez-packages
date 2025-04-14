bba1e9b99705e93973c4bf99b6cf878d1a548420
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2025-04-14 12:37:34 +0100

    add boost-1.85.0

---
+++ boost/1.85.0/build.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+export PACKAGE_INSTALLATION_ROOT=${PACKAGE_INSTALLATION_ROOT:-/mnt/ala/software/ext_packages}
+
+export PACKAGE_NAME=boost
+export PACKAGE_VERSION=1.85.0
+
+mkdir -p $PACKAGE_INSTALLATION_ROOT/$PACKAGE_NAME/$PACKAGE_VERSION
+
+cd boost_1_85_0
+./bootstrap.sh --prefix=$PACKAGE_INSTALLATION_ROOT/$PACKAGE_NAME/$PACKAGE_VERSION
+./b2 --toolset=gcc release install
+cd ..
+cp package.py $PACKAGE_INSTALLATION_ROOT/$PACKAGE_NAME/$PACKAGE_VERSION
+
---
+++ boost/1.85.0/setup.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+wget https://sourceforge.net/projects/boost/files/boost/1.85.0/boost_1_85_0.zip
+unzip boost_1_85_0.zip
